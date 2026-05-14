9eb3dcfea5116840ac99c489c5e3101edd9d614e
giffgaff <mysimcc@gmail.com>
2026-05-15 06:01:43 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('TWILIO_ACCOUNT_SID',  'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+define('TWILIO_AUTH_TOKEN',   'your_auth_token_here');
+define('TWILIO_PHONE_NUMBER', '+1xxxxxxxxxx'); // 你的 Twilio 号码
+define('DB_PATH',             __DIR__ . '/data/sms.db');
+define('ADMIN_PASSWORD',      'your_password_here');
+define('SESSION_KEY',         'twilio_sms_admin');
+?>
